ee33ea54d5dc75fab1a0bd4a92be576f43c7df46
juniodbl <juniodbl@gmail.com>
2025-05-17 22:04:45 -0300

    feat: Implement authentication middleware in api_proxy and update Docker Compose configuration

--- api_proxy/app.js
+++ api_proxy/app.js
@@ -6,9 +6,31 @@ const JWK_URI = process.env.JWK_URI || 'http://localhost:8000/realms/apps/protoc
 const ISSUER = process.env.ISSUER || 'http://localhost:8000/realms/apps';
 const AUDIENCE = process.env.AUDIENCE || 'app';
 const TARGET_API = process.env.TARGET_API || 'http://192.168.2.185:3000/';
+const ENABLE_AUTH = process.env.ENABLE_AUTH || 'true';
 
 const app = express();
 const JWKS = jose.createRemoteJWKSet(new URL(JWK_URI));
+
+const middlewareNoAuth = async (proxyReq, req, res) => {
+    proxyReq.removeHeader('Authorization');
+}
+
+const middlewareAuth = async (proxyReq, req, res) => {
+    const auth = req.headers.authorization;
+    proxyReq.removeHeader('Authorization');
+
+    try {
+        const jwt = auth.split(' ')[1];
+        await validate_token(jwt);
+    } catch (error) {
+        console.log('Token validation error:', error.message);
+        if (!res.headersSent) {
+            res.status(401).json({ error: 'Unauthorized' });
+        }
+        proxyReq.destroy();
+        return;
+    }
+}
 
 async function validate_token(jwt) {
     await jose.jwtVerify(jwt, JWKS, {
@@ -27,24 +49,7 @@ async function validate_token(jwt) {
 app.use('/', createProxyMiddleware({
     target: TARGET_API,
     changeOrigin: false,
-    onProxyReq: async (proxyReq, req, res) => {
-        const auth = req.headers.authorization;
-        proxyReq.removeHeader('Authorization');
-
-        if (auth) {
-            try {
-                const jwt = auth.split(' ')[1];
-                await validate_token(jwt);
-            } catch (error) {
-                console.log('Token validation error:', error.message);
-                if (!res.headersSent) {
-                    res.status(401).json({ error: 'Unauthorized' });
-                }
-                proxyReq.destroy();
-                return;
-            }
-        }
-    },
-}));
+    onProxyReq: ENABLE_AUTH === 'true' ? middlewareAuth : middlewareNoAuth,},
+));
 
 app.listen(3001);
