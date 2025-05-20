2f97c4f342ca910b75f2349376161820b5e6eef0
juniodbl <juniodbl@gmail.com>
2025-05-20 01:13:19 -0300

    chore: Update Estabelicimento component to use useGetMany for fetching multiple establishments

--- cnpj-data-admin/src/components/empresa/empresa.show.jsx
+++ cnpj-data-admin/src/components/empresa/empresa.show.jsx
@@ -1,50 +1,52 @@
-import { DateField, FunctionField, ReferenceField, Show, SimpleShowLayout, TextField, useGetOne, useRecordContext } from 'react-admin';
+import { DateField, FunctionField, ReferenceField, Show, SimpleShowLayout, TextField, useGetMany, useRecordContext } from 'react-admin';
 
 const Estabelicimento = () => {
     const record = useRecordContext();
-    const { data: estabelecimento, isLoading, error } = useGetOne('estabelecimento', { id: record.cnpj_basico });
+    const { data: estabelecimentos, isLoading, error } = useGetMany('estabelecimento', { id: record.cnpj_basico });
 
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>Error...</p>;
 
-    return (
-        <SimpleShowLayout record={estabelecimento}>
-            <h2>Estabelecimento</h2>
+    return <>
+        {estabelecimentos.map(estabelecimento => (
+            <SimpleShowLayout record={estabelecimento}>
+                <h2>Estabelecimento</h2>
 
-            <FunctionField label="CNPJ" render={record => `${record.cnpj_basico}-${record.cnpj_ordem}/${record.cnpj_dv}`} />
+                <FunctionField label="CNPJ" render={record => `${record.cnpj_basico}-${record.cnpj_ordem}/${record.cnpj_dv}`} />
 
-            <TextField source="identificador_matriz_filial" />
+                <TextField source="identificador_matriz_filial" />
 
-            <TextField source="nome_fantasia" />
+                <TextField source="nome_fantasia" />
 
-            <TextField source="situacao_cadastral" />
-            <DateField source="data_situacao_cadastral" transform={v => v ? new Date(v) : null} />
+                <TextField source="situacao_cadastral" />
+                <DateField source="data_situacao_cadastral" transform={v => v ? new Date(v) : null} />
 
-            <TextField source="motivo_situacao_cadastral" />
-            <TextField source="nome_cidade_exterior" />
-            <TextField source="pais" />
-            <TextField source="data_inicio_atividade" />
-            <TextField source="cnae_fiscal_principal" />
-            <TextField source="cnae_fiscal_secundaria" />
-            <TextField source="tipo_logradouro" />
-            <TextField source="logradouro" />
-            <TextField source="numero" />
-            <TextField source="complemento" />
-            <TextField source="bairro" />
-            <TextField source="cep" />
-            <TextField source="uf" />
-            <TextField source="municipio" />
-            <TextField source="ddd_1" />
-            <TextField source="telefone_1" />
-            <TextField source="ddd_2" />
-            <TextField source="telefone_2" />
-            <TextField source="ddd_fax" />
-            <TextField source="fax" />
-            <TextField source="correio_eletronico" />
-            <TextField source="situacao_especial" />
-            <TextField source="data_situacao_especial" />
-        </SimpleShowLayout>
-    );
+                <TextField source="motivo_situacao_cadastral" />
+                <TextField source="nome_cidade_exterior" />
+                <TextField source="pais" />
+                <TextField source="data_inicio_atividade" />
+                <TextField source="cnae_fiscal_principal" />
+                <TextField source="cnae_fiscal_secundaria" />
+                <TextField source="tipo_logradouro" />
+                <TextField source="logradouro" />
+                <TextField source="numero" />
+                <TextField source="complemento" />
+                <TextField source="bairro" />
+                <TextField source="cep" />
+                <TextField source="uf" />
+                <TextField source="municipio" />
+                <TextField source="ddd_1" />
+                <TextField source="telefone_1" />
+                <TextField source="ddd_2" />
+                <TextField source="telefone_2" />
+                <TextField source="ddd_fax" />
+                <TextField source="fax" />
+                <TextField source="correio_eletronico" />
+                <TextField source="situacao_especial" />
+                <TextField source="data_situacao_especial" />
+            </SimpleShowLayout>
+        ))}
+    </>;
 }
 
 
